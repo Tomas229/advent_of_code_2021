e7549b6fc9ed26d35f97edacbf6d12d94979b5d1
Tomas229 <53621992+Tomas229@users.noreply.github.com>
2021-12-04 19:38:27 -0300

    Day 01 2nd Star
    05:41

--- day_01/Main.java
+++ day_01/Main.java
@@ -4,13 +4,28 @@ import java.nio.file.*;
 
 class Main {
   public static void main(String[] args) throws IOException {
-
     List<String> content = Files.readAllLines(Paths.get("input.txt"));
 
-    int count = 0;
+    System.out.println(content);
+
     // int depths[] = { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+
+    int count = 0;
     for (int i = 0; i < content.size() - 1; i++) {
-      if (Integer.parseInt(content.get(i)) < Integer.parseInt(content.get(i+1))) {
+      if (Integer.parseInt(content.get(i)) < Integer.parseInt(content.get(i + 1))) {
+        count++;
+      }
+    }
+    System.out.println(count);
+
+    count = 0;
+    for (int i = 0; i < content.size() - 3; i++) {
+      int first_window = Integer.parseInt(content.get(i)) + Integer.parseInt(content.get(i + 1))
+          + Integer.parseInt(content.get(i + 2));
+      int second_window = Integer.parseInt(content.get(i + 1)) + Integer.parseInt(content.get(i + 2))
+          + Integer.parseInt(content.get(i + 3));
+
+      if (first_window < second_window) {
         count++;
       }
     }
