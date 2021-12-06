876d6dd388413ec544978f0de417594626fab474
Tomas229 <53621992+Tomas229@users.noreply.github.com>
2021-12-05 21:16:51 -0300

    Day 03 2nd Star
    01:25:00

--- day_03/Main.java
+++ day_03/Main.java
@@ -4,12 +4,12 @@ import java.nio.file.*;
 import java.util.*;
 
 class Main {
+  public int index = 0;
+
   public static void main(String[] args) throws IOException {
     List<String> content = Files.readAllLines(Paths.get("input.txt"));
 
     // System.out.println(content);
-
-    // int depths[] = { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
 
     int gamma[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
@@ -35,9 +35,63 @@ class Main {
       }
     }
 
-    System.out.println(s);
     int num = Integer.parseInt(s, 2);
 
     System.out.println(num * (num ^ Integer.parseInt("111111111111", 2)));
+
+    System.out.println(Integer.parseInt(Main.major_list(new ArrayList<String>(content)).get(0), 2)
+        * Integer.parseInt(Main.minor_list(new ArrayList<String>(content)).get(0), 2));
+  }
+
+  public static List<String> major_list(List<String> l) {
+    int i = 0;
+    while (l.size() > 1) {
+      List<String> List_0 = new ArrayList<String>();
+      List<String> List_1 = new ArrayList<String>();
+
+      for (String code : l) {
+        if (code.charAt(i) == '0') {
+          List_0.add(code);
+        } else {
+          List_1.add(code);
+        }
+      }
+      i = i + 1;
+
+      if (List_0.size() > List_1.size()) {
+        l.clear();
+        l.addAll(List_0);
+      } else {
+        l.clear();
+        l.addAll(List_1);
+      }
+    }
+    return l;
+  }
+
+  public static List<String> minor_list(List<String> l) {
+    int i = 0;
+    while (l.size() > 1) {
+      List<String> List_0 = new ArrayList<String>();
+      List<String> List_1 = new ArrayList<String>();
+
+      for (String code : l) {
+        if (code.charAt(i) == '0') {
+          List_0.add(code);
+        } else {
+          List_1.add(code);
+        }
+      }
+      i = i + 1;
+
+      if (List_0.size() <= List_1.size()) {
+        l.clear();
+        l.addAll(List_0);
+      } else {
+        l.clear();
+        l.addAll(List_1);
+      }
+    }
+    return l;
   }
 }
