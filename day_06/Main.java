0c0f3143b9f41df818a545f101a5ccb3ac441be1
Tomas229 <53621992+Tomas229@users.noreply.github.com>
2021-12-06 23:58:42 -0300

    Day 06 2nd Star

--- day_06/Main.java
+++ day_06/Main.java
@@ -12,9 +12,10 @@ class Main {
     String[] init = input.get(0).split(",");
 
     Main.first_star(init);
+    Main.second_star(init);
   }
 
-  public static void first_star(String [] init){
+  public static void first_star(String[] init) {
     List<Lanternfish> fishes = new ArrayList<Lanternfish>();
     for (int i = 0; i < init.length; i++) {
       fishes.add(new Lanternfish(Integer.parseInt(init[i])));
@@ -30,6 +31,21 @@ class Main {
       fishes.addAll(aux);
     }
     System.out.println(fishes.size());
+  }
+
+  public static void second_star(String[] init) {
+    int[] fishes = new int[9];
+    for (int i = 0; i < init.length; i++) {
+      fishes[Integer.parseInt(init[i])] = fishes[Integer.parseInt(init[i])] + 1;
+    }
+
+    Fish f = new Fish(fishes);
+
+    for( int j = 0; j < 256; j++){
+      f.newDay();
+    }
+    System.out.println(f.total());
+
   }
 }
 
@@ -51,5 +67,47 @@ class Lanternfish {
     }
     this.timer = (this.timer - 1);
     return false;
+  }
+}
+
+class Fish {
+  long fish_0;
+  long fish_1;
+  long fish_2;
+  long fish_3;
+  long fish_4;
+  long fish_5;
+  long fish_6;
+  long fish_7;
+  long fish_8;
+
+  public Fish(int[] input) {
+    this.fish_0 = input[0];
+    this.fish_1 = input[1];
+    this.fish_2 = input[2];
+    this.fish_3 = input[3];
+    this.fish_4 = input[4];
+    this.fish_5 = input[5];
+    this.fish_6 = input[6];
+    this.fish_7 = input[7];
+    this.fish_8 = input[8];
+  }
+
+  public void newDay() {
+    long fish_0_aux = this.fish_0;
+    this.fish_0 = this.fish_1;
+    this.fish_1 = this.fish_2;
+    this.fish_2 = this.fish_3;
+    this.fish_3 = this.fish_4;
+    this.fish_4 = this.fish_5;
+    this.fish_5 = this.fish_6;
+    this.fish_6 = this.fish_7 + fish_0_aux;
+    this.fish_7 = this.fish_8;
+    this.fish_8 = fish_0_aux;
+  }
+
+  public long total() {
+    return this.fish_0 + this.fish_1 + this.fish_2 + this.fish_3 + this.fish_4 + this.fish_5 + this.fish_6 + this.fish_7
+        + this.fish_8;
   }
 }
