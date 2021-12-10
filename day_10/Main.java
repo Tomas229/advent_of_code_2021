df774598280af31f73e538aafa98e1659f48db76
Tomas229 <53621992+Tomas229@users.noreply.github.com>
2021-12-10 14:42:12 -0300

    Day 10 2nd Star

--- day_10/Main.java
+++ day_10/Main.java
@@ -7,8 +7,10 @@ import java.util.Arrays;
 
 class Main {
   public static Map<Character, Character> map = new HashMap<Character, Character>();
-  public static Map<Character, Integer> score = new HashMap<Character, Integer>();
+  public static Map<Character, Character> reverseMap = new HashMap<Character, Character>();
+  public static Map<Character, Integer> corruptedScore = new HashMap<Character, Integer>();
 
+  public static Map<Character, Integer> incompleteScore = new HashMap<Character, Integer>();
 
   public static void main(String[] args) throws IOException {
     List<String> input = Main.getMultiLineInputStringArray();
@@ -18,13 +20,23 @@ class Main {
     Main.map.put('}', '{');
     Main.map.put('>', '<');
 
-    Main.score.put(')', 3);
-    Main.score.put(']', 57);
-    Main.score.put('}', 1197);
-    Main.score.put('>', 25137);
+    Main.reverseMap.put('(', ')');
+    Main.reverseMap.put('[', ']');
+    Main.reverseMap.put('{', '}');
+    Main.reverseMap.put('<', '>');
+
+    Main.corruptedScore.put(')', 3);
+    Main.corruptedScore.put(']', 57);
+    Main.corruptedScore.put('}', 1197);
+    Main.corruptedScore.put('>', 25137);
+
+    Main.incompleteScore.put(')', 1);
+    Main.incompleteScore.put(']', 2);
+    Main.incompleteScore.put('}', 3);
+    Main.incompleteScore.put('>', 4);
 
     Main.printFirstStarSolution(input);
-    // Main.printSecondStarSolution(input);
+    Main.printSecondStarSolution(input);
   }
 
   public static int[] getOneLineInputIntArray() throws IOException {
@@ -60,27 +72,66 @@ class Main {
     int sum = 0;
 
     for (String line : init) {
-      List<Character> actual = new ArrayList<Character>();
-      actual.add(line.charAt(0));
-
-      for (int i = 1, n = line.length(); i < n; i++) {
-        char c = line.charAt(i);
-        if (c == '(' || c == '[' || c == '{' || c == '<') {
-          actual.add(c);
-        } else {
-          if (map.get(c) == actual.get(actual.size() - 1)) {
-            actual.remove(actual.size() - 1);
-          } else {
-            sum = sum + score.get(c);
-            break;
-          }
-        }
-      }
+      sum = sum + Main.getCorruptedScore(line);
     }
 
     System.out.println(sum);
   }
 
   public static void printSecondStarSolution(List<String> init) {
+    List<Long> points = new ArrayList<Long>();
+    for (String line : init) {
+      long score = Main.getIncompleteScore(line);
+      if (score != 0) {
+        points.add(score);
+      }
+    }
+    Collections.sort(points);
+    System.out.println(points.get(points.size() / 2));
+  }
+
+  public static int getCorruptedScore(String line) {
+    List<Character> actual = new ArrayList<Character>();
+    actual.add(line.charAt(0));
+
+    for (int i = 1, n = line.length(); i < n; i++) {
+      char c = line.charAt(i);
+      if (c == '(' || c == '[' || c == '{' || c == '<') {
+        actual.add(c);
+      } else {
+        if (map.get(c) == actual.get(actual.size() - 1)) {
+          actual.remove(actual.size() - 1);
+        } else {
+          return corruptedScore.get(c);
+        }
+      }
+    }
+    return 0;
+  }
+
+  public static Long getIncompleteScore(String line) {
+    List<Character> actual = new ArrayList<Character>();
+    actual.add(line.charAt(0));
+
+    Long sum = 0L;
+
+    for (int i = 1, n = line.length(); i < n; i++) {
+      char c = line.charAt(i);
+      if (c == '(' || c == '[' || c == '{' || c == '<') {
+        actual.add(c);
+      } else {
+        if (map.get(c) == actual.get(actual.size() - 1)) {
+          actual.remove(actual.size() - 1);
+        } else {
+          return 0L;
+        }
+      }
+    }
+    for (int i = 0; i < actual.size(); i++) {
+      sum = sum * 5;
+      char r = Main.reverseMap.get(actual.get(actual.size() - i - 1));
+      sum = sum + Main.incompleteScore.get(r);
+    }
+    return sum;
   }
 }
